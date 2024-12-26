5938ab3673f0bec85ebdf977df868c27717084c3
DucHuy74 <duchuyhl74205@gmail.com>
2024-12-26 09:48:36 +0700

    add prj

---
+++ src/test/java/com/proj/tmdt/TmdtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.tmdt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmdtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
