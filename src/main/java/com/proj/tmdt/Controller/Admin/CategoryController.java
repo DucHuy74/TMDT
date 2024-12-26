0ed4adbc27c28dd5ba1485f81c1ed236dc599115
DucHuy74 <duchuyhl74205@gmail.com>
2024-12-26 11:57:10 +0700

    add admin UI

---
+++ src/main/java/com/proj/tmdt/Controller/Admin/CategoryController.java
@@ -0,0 +1,19 @@
+package com.proj.tmdt.Controller.Admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class CategoryController {
+    @RequestMapping("admin/category")
+    public String index(){
+        return "admin/category/index";
+    }
+
+    @RequestMapping("admin/add-category")
+    public String addCategory(){
+        return "admin/category/add";
+    }
+}
